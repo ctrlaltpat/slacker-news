6e93370ac949ff9c0149706c31096cca03182720
Patrick <ctrlaltpat@gmail.com>
2019-01-26 17:49:52 +0000

    fixed CORS issue... app.use(cors) -> app.use(cors())

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ const storiesRouter = require('./routes/stories');
 const app = express();
 
 app.use(logger('dev'));
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
